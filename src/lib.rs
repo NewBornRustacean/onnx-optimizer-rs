bef2fd2ac50e4a07325287988fa32a6aa230b8fb
NewBornRustacean <huiseomkim@gmail.com>
2025-08-08 22:43:51 +0000

    build proto from official onnx.proto description

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 mod error;
 mod utils;
 mod graph;
+mod io;
+
+pub mod proto {
+    include!(concat!(env!("OUT_DIR"), "/onnx.rs"));
+}
 
 pub use graph::*;
 pub use error::OnnxOptError;
+pub use io::*;
